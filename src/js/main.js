76df345ae75e71e372c9703dbc56873b4333f920
Lareta064 <lareta064@gmail.com>
2022-03-19 23:03:31 +0300

    slider-plan

--- src/js/main.js
+++ src/js/main.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function (){
        slidesPerView: 1,
         speed:800,
         loop: true,
-        centeredSlides: true,
+        spaceBetween: 16,
          navigation: {
           nextEl: ".portfolio-slider-next",
           prevEl: ".portfolio-slider-prev",
@@ -99,6 +99,65 @@ document.addEventListener("DOMContentLoaded", function (){
       }
     });
 
+    /*    catalog tabs */
+    let catalogTabs = new Swiper(".catalog-tabs-btns", {
+       slidesPerView: 2,
+        speed:800,
+        
+         navigation: {
+          nextEl: ".catalog-tabs-next",
+          prevEl: ".catalog-tabs-prev",
+        },
+        breakpoints: {
+				455: {
+					slidesPerView: 2.8
+				},
+				768:{
+					slidesPerView: 4
+				},
+				1024:{
+					slidesPerView: 5
+				},
+        1200:{
+          slidesPerView: 6,
+        },
+         1365:{
+          slidesPerView: 8,
+        }
+			}
+    });
+
+    /*====slider single project =========*/
+    var swiper = new Swiper(".mySwiper", {
+        loop: true,
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+      });
+      var swiper2 = new Swiper(".mySwiper2", {
+        loop: true,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".mySwiper2-next",
+          prevEl: ".mySwiper2-prev",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
+
+      /*====slider project plan mobile =========*/
+
+      let projectPlan = new Swiper(".project-variants-slider", {
+       slidesPerView: 1,
+        speed:800,
+        loop:true,
+         navigation: {
+          nextEl: ".project-variants-next",
+          prevEl: ".project-variants-prev",
+        }
+      });
      /* modal*/ 
     const modalFramesOpen = document.querySelectorAll('[frame-btn]');
     const modalFrames = document.querySelectorAll('[frame-modal]');
@@ -144,4 +203,41 @@ document.addEventListener("DOMContentLoaded", function (){
       bodyEl.classList.remove('noscroll');
       this.classList.remove('active');
     });
+
+    /*===== Catalog  табы  ===== */
+    const customTabs = document.querySelectorAll('[custom-tabs]');
+    
+    if(customTabs.length > 0){
+      for(let item of customTabs){
+        
+        const ctBtns = item.querySelectorAll('[ct-btn]');
+        const ctContents = item.querySelectorAll('[ct-content]');
+        
+        for(let i=0; i< ctBtns.length; i++){
+          ctBtns[i].addEventListener('click', function(){
+            for(let j=0; j< ctBtns.length;j++){
+              if(j!=i){
+                ctBtns[j].classList.remove('custom-tab--current');
+                
+              }
+              else{
+                const thisData =  this.getAttribute('ct-btn');
+                this.classList.add('custom-tab--current');
+                
+
+                for(let content of ctContents){
+                  content.classList.remove('ct-content--active');
+                  const contentData = content.getAttribute('ct-content');
+                  if(contentData == thisData){
+                    content.classList.add('ct-content--active');
+                  }
+                }
+              }
+
+            }
+            
+          });
+        }
+      }
+    }
 })
